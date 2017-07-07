deccab30333a8633b674d9e5bb1489d090148281
eman000tahaz <emey_gif@yahoo.com>
2017-07-07 19:55:47 +0000

    ya rb

---
+++ odoo-server/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo -u odoo /odoo/odoo-server/openerp-server --config=/etc/odoo-server.conf
+#!/bin/sh
+sudo -u odoo /odoo/odoo-server/openerp-server --config=/etc/odoo-server.conf
+#!/bin/sh
+sudo -u odoo /odoo/odoo-server/openerp-server --config=/etc/odoo-server.conf
+#!/bin/sh
+sudo -u odoo /odoo/odoo-server/openerp-server --config=/etc/odoo-server.conf
